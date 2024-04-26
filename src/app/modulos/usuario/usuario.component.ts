be10d2fc14530f0b154382253db83b0b0b224274
santiagoox8 <santiagoox8@gmail.com>
2024-04-25 21:43:56 -0500

    5.0 version

--- src/app/modulos/usuario/usuario.component.ts
+++ src/app/modulos/usuario/usuario.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from 'src/app/servicios/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-usuario',
@@ -11,11 +12,23 @@ export class UsuarioComponent implements OnInit {
   //variables globales
   verf= false;
   usuario: any;
+  user = {
+    nombre: "",
+    email:"",
+    clave:"",
+    tipo:""
+  };
+  //para validar
+  validnombre = true;
+  validemail = true;
+  validclave = true;
+  validtipo = true;
 
   constructor(private suser: UsuarioService) { }
 
   ngOnInit(): void {
     this.consulta();
+    this.limpiar();
   }
   //mostrar formulario
   mostrar(dato:any) {
@@ -28,14 +41,93 @@ export class UsuarioComponent implements OnInit {
       break;
     }
   }
+  //limpiar
+  limpiar(){
+    this.user.nombre = "";
+    this.user.email = "";
+    this.user.clave = "";
+    this.user.tipo = "";
+  }
+  //validar
+  validar(){
+    if(this.user.nombre == ""){
+      this.validnombre = false;
+    }else{
+      this.validnombre = true;
+    }
+
+    if(this.user.email == ""){
+      this.validemail = false;
+    }else{
+      this.validemail = true;
+    }
+
+    if(this.user.clave == ""){
+      this.validclave = false;
+    }else{
+      this.validclave = true;
+    }
+
+    if(this.user.tipo == ""){
+      this.validtipo = false;
+    }else{
+      this.validtipo = true;
+    }
+  }
 
 
   consulta() {
     this.suser.consultar().subscribe((result:any) => {
       this.usuario = result;
-      console.log(this.usuario);
+      //console.log(this.usuario);
     })
   }
 
+  ingresar() {
+    //console.log(this.cat);
+    this.validar();
+
+    if(this.validnombre=true && this.validemail==true && this.validclave==true && this.validtipo==true){
+      this.suser.insertar(this.user).subscribe((datos:any) => {
+        if (datos['resultado']=='OK') {
+          //alert(datos['mensaje']);
+          this.consulta();
+        }
+      });
+      this.mostrar(0);
+      this.limpiar();
+    }
+  }
+
+  pregunta (id: any, nombre: any) {
+    console.log("entro con el id " + id);
+    Swal.fire({
+      title: 'Esta seguro de eliminar el usuario '+ nombre +'?',
+      text: "Esta accion no sera revertida!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, eliminar!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.borrarusuario(id);
+        Swal.fire({
+          title: "Eliminado!",
+          text: "El usuario ha sido eliminado",
+          icon: "success"
+        });
+      }
+    });
+  }
+
+  borrarusuario (id:any){
+    console.log(id);
+    this.suser.eliminar(id).subscribe((datos:any) => {
+      if(datos['resultado']=='OK'){
+        this.consulta();
+      }
+    });
+  }
 }
 
